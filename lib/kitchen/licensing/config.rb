56040d1e2d9d4818879ad1eafccf5354171f0577
nitin sanghi <nsanghi@progress.com>
2025-02-12 18:07:07 +0530

    Extra chef name remove for license message
    Signed-off-by: nitin sanghi <nsanghi@progress.com>

--- lib/kitchen/licensing/config.rb
+++ lib/kitchen/licensing/config.rb
@@ -19,7 +19,7 @@
 require "chef-licensing"
 
 ChefLicensing.configure do |config|
-  config.chef_product_name = "Chef Test Kitchen Enterprise"
+  config.chef_product_name = "Test Kitchen Enterprise"
   config.chef_entitlement_id = "x6f3bc76-a94f-4b6c-bc97-4b7ed2b045c0"
   config.chef_executable_name = "kitchen"
   config.license_server_url = "https://services.chef.io/licensing"
